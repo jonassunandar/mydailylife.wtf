00b759066fa1fe5c1422d84f8bf2bc7f5ad51e9b
Akramurridjal Rahman <xerod@outlook.com>
2021-08-13 17:18:06 +0700

    ✨ App able to connect to metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
-import React, { useState } from "react";
+import MetaMaskOnboarding from "@metamask/onboarding";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const ONBOARD_TEXT = "Click here to install MetaMask!";
+const CONNECT_TEXT = "Connect";
+const CONNECTED_TEXT = "Connected";
 
-  return <div className="App"></div>;
+declare let window: any;
+
+export function App() {
+  const [buttonText, setButtonText] = React.useState(ONBOARD_TEXT);
+  const [isDisabled, setDisabled] = React.useState(false);
+  const [accounts, setAccounts] = React.useState([]);
+  const onboarding = React.useRef<MetaMaskOnboarding>();
+
+  React.useEffect(() => {
+    if (!onboarding.current) {
+      onboarding.current = new MetaMaskOnboarding();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      if (accounts.length > 0) {
+        setButtonText(CONNECTED_TEXT);
+        setDisabled(true);
+        onboarding.current?.stopOnboarding();
+      } else {
+        setButtonText(CONNECT_TEXT);
+        setDisabled(false);
+      }
+    }
+  }, [accounts]);
+
+  const onClick = () => {
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((newAccounts: any) => setAccounts(newAccounts));
+    } else {
+      onboarding.current?.startOnboarding();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <button disabled={isDisabled} onClick={onClick}>
+          {buttonText}
+        </button>
+        <p>Accounts: {accounts}</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
